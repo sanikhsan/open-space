efd9949706374bfdd06757768ff23387f1510744
San Ikhsan <san.projects@gdrive.biz.id>
2023-06-26 04:01:13 +0700

    Create Action and Reducer for State isPreload

--- src/states/isPreload/action.js
+++ src/states/isPreload/action.js
@@ -1,3 +1,38 @@
 /**
  * @TODO: Define all the actions (creator) for the isPreLoad state
  */
+
+import api from '../../utils/api';
+import { setAuthUserActionCreator } from '../authUser/action';
+
+const ActionType = {
+  SET_IS_PRELOAD: 'SET_IS_PRELOAD',
+};
+
+function setIsPreloadActionCreator(isPreLoad) {
+  return {
+    type: ActionType.SET_IS_PRELOAD,
+    payload: {
+      isPreLoad,
+    },
+  };
+}
+
+// Fungsi Thunk
+function asyncPreloadProcess() {
+  return async (dispatch) => {
+    try {
+      // proses preload
+      const authUser = await api.getOwnProfile();
+      dispatch(setAuthUserActionCreator(authUser));
+    } catch (error) {
+      // proses fallback
+      dispatch(setAuthUserActionCreator(null));
+    } finally {
+      // end proses preload
+      dispatch(setIsPreloadActionCreator(false));
+    }
+  };
+}
+
+export { ActionType, setIsPreloadActionCreator, asyncPreloadProcess };
